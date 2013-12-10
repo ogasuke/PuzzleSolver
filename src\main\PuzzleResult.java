aaf7ddbd424dbdde0e86164ad2a3fc2127e13a43
ogasuke <ogyasuke@yahoo.co.jp>
2013-12-10 21:06:04 +0900

    Create src\main\PuzzleResult.java

---
+++ src\main\PuzzleResult.java
@@ -0,0 +1,22 @@
+package main;
+
+/**
+ * パズルの結果を表現します。
+ * @author ogasawara
+ */
+public enum PuzzleResult {
+
+  SUCCESS("成功！"),
+  CONTINUE("途中で処理が終わりました。"),
+  FAIRURE("全ルート探索しましたが、解けないです。");
+
+  private PuzzleResult(String message) {
+    this.message = message;
+  }
+
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+}
